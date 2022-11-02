37fc5a14b2d1d33e2c4ffbbaaf1ee4dd6c4b7811
Janis Ringli <janis.ringli@gmail.com>
2022-11-02 16:33:32 +0100

    init

---
+++ house-of-tichu/house_of_tichuApp.swift
@@ -0,0 +1,17 @@
+//
+//  house_of_tichuApp.swift
+//  house-of-tichu
+//
+//  Created by Janis Ringli on 02.11.22.
+//
+
+import SwiftUI
+
+@main
+struct house_of_tichuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
